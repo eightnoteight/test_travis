5de79df04258fd3f460b5ec408525b9a2a7aa843
eightnoteight <mr.eightnoteight@gmail.com>
2016-02-29 16:59:54 +0530

    blah

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import unittest
 class TestStringMethods(unittest.TestCase):
     def test_bfs(self):
         import bsearch
-        self.assertEqual(bsearch.bsearch(0, 10, lambda x: x >= 6), 4)
+        self.assertEqual(bsearch.bsearch(0, 10, lambda x: x >= 6), 2)
 
